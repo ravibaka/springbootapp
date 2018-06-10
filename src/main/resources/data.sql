1ed7a16ad5146df9139caad113be4a4e585adb95
ravibaka <bakaravi.c@gmail.com>
2018-06-10 23:39:46 +0200

    Updated data for JPA mapping

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into customer
-values(10001,'Manju', 'manju@gmail.com', 'ACTIVE');
+values(10001,'Kristoffer', 'kristoffer@email.com', 'ACTIVE');
 
 insert into customer
-values(10002,'Ravi', 'ravi@gmail.com', 'REJECTED');
+values(10002,'Erik', 'erik@email.com', 'REGISTERED');
 
 insert into customer
-values(10003,'Suchi', 'suchi@gmail.com', 'PENDING');
+values(10003,'Andre', 'andre@email.com', 'REGISTERED');
 
 insert into customer
-values(10004,'Test', 'test@gmail.com', 'REGISTERED');
+values(10004,'Ravi', 'ravi@email.com', 'PENDING');
